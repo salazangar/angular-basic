3bebcb1e8f173e4a5e568819b60f873504026166
salazangar <sidharthm673@gmail.com>
2024-08-28 01:15:12 +0530

    removing initial stuff

--- my-first-angular-project/src/app/app.component.ts
+++ my-first-angular-project/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'my first app';
+
+  firstName = "Mike";
+  lastName = "Tyson";
 }
