9c86640f725ec6f6e0dfaa89ecffcb21018707e4
salazangar <sidharthm673@gmail.com>
2024-08-28 13:41:20 +0530

    adding table

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  // create an array of object
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anup", "Kumar", "anup@gmail.com", 40000),
+    new SalesPerson("Mike", "Tyson", "mike@gmail.com", 5000),
+    new SalesPerson("Sarah", "Powell", "sarah@gmail.com", 35000),
+    new SalesPerson("Jain", "Kant", "jain@gmail.com", 70000),
+    new SalesPerson("Margot", "Pierce", "mauap@gmail.com", 20000),
+  ];
 
   constructor() { }
 
